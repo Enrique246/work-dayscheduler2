6b6d2772091c9d44401fd5c2a9e39dde3735965f
Henri <ecch9210@gmail.com>
2021-04-20 17:13:16 -0500

    Workday JavaScript finished

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,3 @@
-// Local Storage variables
-
-// // Store
-//localStorage.setItem("lastname", "Smith");
-// Retrieve
-//document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-//https://www.w3schools.com/htmL/html5_webstorage.asp
-
-let tArea9Storage = localStorage.getItem('h9');
-let tArea10Storage = localStorage.getItem('h10');
-let tArea11Storage = localStorage.getItem('h11');
-let tArea12Storage = localStorage.getItem('h12');
-let tArea1Storage = localStorage.getItem('h1');
-let tArea2Storage = localStorage.getItem('h2');
-let tArea3Storage = localStorage.getItem('h3');
-let tArea4Storage = localStorage.getItem('h4');
-let tArea5Storage = localStorage.getItem('h5');
-
-// Id's for variables
-
-//Calling jQuery() (or $()) with an id selector as its argument will return a jQuery object containing a collection of either zero or one DOM element.
-//https://api.jquery.com/id-selector/
-
-let tArea9 = $('#h9');
-let tArea10 = $('#h10');
-let tArea11 = $('#h11');
-let tArea12 = $('#h12');
-let tArea1 = $('#h1');
-let tArea2 = $('#h2');
-let tArea3 = $('#h3');
-let tArea4 = $('#h4');
-let tArea5 = $('#h5');
-
-// Value variables
-//The .val() method is primarily used to get the values of form elements such as input, select and textarea. When called on an empty collection, it returns undefined.
-// https://api.jquery.com/val/#val
-tArea9.val (tArea9Storage);
-tArea10.val (tArea10Storage);
-tArea11.val (tArea11Storage);
-tArea12.val (tArea12Storage);
-tArea1.val (tArea1Storage);
-tArea2.val (tArea2Storage);
-tArea3.val (tArea3Storage);
-tArea4.val (tArea4Storage);
-tArea5.val (tArea5Storage);
-
 // Current day query
 let cDate = moment().format("dddd, MMMM Do, YYYY")
 
@@ -105,3 +59,48 @@ localStorage.setItem(variableT, textContent);
 
 });
 
+// Local Storage variables
+
+// // Store
+//localStorage.setItem("lastname", "Smith");
+// Retrieve
+//document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+//https://www.w3schools.com/htmL/html5_webstorage.asp
+
+let tArea9Storage = localStorage.getItem('h9');
+let tArea10Storage = localStorage.getItem('h10');
+let tArea11Storage = localStorage.getItem('h11');
+let tArea12Storage = localStorage.getItem('h12');
+let tArea1Storage = localStorage.getItem('h1');
+let tArea2Storage = localStorage.getItem('h2');
+let tArea3Storage = localStorage.getItem('h3');
+let tArea4Storage = localStorage.getItem('h4');
+let tArea5Storage = localStorage.getItem('h5');
+
+// Id's for variables
+
+//Calling jQuery() (or $()) with an id selector as its argument will return a jQuery object containing a collection of either zero or one DOM element.
+//https://api.jquery.com/id-selector/
+
+let tArea9 = $('#h9');
+let tArea10 = $('#h10');
+let tArea11 = $('#h11');
+let tArea12 = $('#h12');
+let tArea1 = $('#h1');
+let tArea2 = $('#h2');
+let tArea3 = $('#h3');
+let tArea4 = $('#h4');
+let tArea5 = $('#h5');
+
+// Value variables
+//The .val() method is primarily used to get the values of form elements such as input, select and textarea. When called on an empty collection, it returns undefined.
+// https://api.jquery.com/val/#val
+tArea9.val (tArea9Storage);
+tArea10.val (tArea10Storage);
+tArea11.val (tArea11Storage);
+tArea12.val (tArea12Storage);
+tArea1.val (tArea1Storage);
+tArea2.val (tArea2Storage);
+tArea3.val (tArea3Storage);
+tArea4.val (tArea4Storage);
+tArea5.val (tArea5Storage);
